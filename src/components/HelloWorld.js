9b964a02ec9fe3c486ce7cefa99836ef313c99a6
kaio05 <98607741+kaio05@users.noreply.github.com>
2023-09-22 20:21:58 -0300

    Primeiro projeto React

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,14 @@
+import Frase from './Frase';
+
+function HelloWorld() {
+
+    return (
+        <div>
+            <Frase/>
+            <h1>We live, we love, we lie</h1>
+        </div>
+    )
+
+}
+
+export default HelloWorld
